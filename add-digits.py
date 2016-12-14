3d08f4bb72dec95b3f35ca48efc2fc8ff5663cbc
wudizhangzhi <wudizhangzhi@163.com>
2016-12-14 17:47:26 +0800

    2016-12-14

--- add-digits.py
+++ add-digits.py
@@ -10,15 +10,24 @@ Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one
 Follow up:
 Could you do it without any loop/recursion in O(1) runtime?
 '''
+
+import operator
 class Solution(object):
     def addDigits(self, num):
         """
         :type num: int
         :rtype: int
         """
+        return num if num<10 else self.addDigits(reduce(lambda x,y:int(x)+int(y) ,list(str(num))))
+
+
 
 
 
 if __name__ == '__main__':
+    import time
+    import random
     sol = Solution()
-    print sol.addDigits()
+    start = time.time()
+    print sol.addDigits(random.randint(1000,9999))
+    print time.time()-start
