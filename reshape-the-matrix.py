3cc8e59cc649b428617bdba3fcc0c8135497b742
张智超 <zhangzhichao@promote.cache-dns.local>
2017-05-22 23:43:30 +0800

    2017-05-21

---
+++ reshape-the-matrix.py
@@ -0,0 +1,71 @@
+# -*- coding:utf8 -*-
+"""
+In MATLAB, there is a very useful function called 'reshape', which can reshape a matrix into a new one with different size but keep its original data.
+
+You're given a matrix represented by a two-dimensional array, and two positive integers r and c representing the row number and column number of the wanted reshaped matrix, respectively.
+
+The reshaped matrix need to be filled with all the elements of the original matrix in the same row-traversing order as they were.
+
+If the 'reshape' operation with given parameters is possible and legal, output the new reshaped matrix; Otherwise, output the original matrix.
+
+Example 1:
+Input:
+nums =
+[[1,2],
+ [3,4]]
+r = 1, c = 4
+Output:
+[[1,2,3,4]]
+Explanation:
+The row-traversing of nums is [1,2,3,4]. The new reshaped matrix is a 1 * 4 matrix, fill it row by row by using the previous list.
+Example 2:
+Input:
+nums =
+[[1,2],
+ [3,4]]
+r = 2, c = 4
+Output:
+[[1,2],
+ [3,4]]
+Explanation:
+There is no way to reshape a 2 * 2 matrix to a 2 * 4 matrix. So output the original matrix.
+Note:
+The height and width of the given matrix is in range [1, 100].
+The given r and c are all positive.
+"""
+
+class Solution(object):
+    def matrixReshape(self, nums, r, c):
+        """
+        :type nums: List[List[int]]
+        :type r: int
+        :type c: int
+        :rtype: List[List[int]]
+        """
+        flat = sum(nums, [])
+        if len(flat) != (r * c):
+            return nums
+        tuples = zip(*([iter(flat)] * c))
+        return map(list, tuples)
+
+    def matrixReshape2(self, nums, r, c):
+        """
+        :type nums: List[List[int]]
+        :type r: int
+        :type c: int
+        :rtype: List[List[int]]
+        """
+        import itertools
+        it = itertools.chain(*nums)
+        return [list(itertools.islice(it, c)) for _ in xrange(r)]
+
+if __name__ == '__main__':
+    nums = [[1, 2],[3, 4]]
+    r = 1
+    c = 4
+
+    sol = Solution()
+    result = sol.matrixReshape(nums, r, c)
+    print result
+    result = sol.matrixReshape2(nums, r, c)
+    print result
