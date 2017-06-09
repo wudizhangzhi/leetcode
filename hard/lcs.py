98e82ec29790c3b56551a5cd8ef3b6c5dc4417b6
wudizhangzhi <zhangzhichao@taidii.com>
2017-06-09 14:33:33 +0800

    lcs

---
+++ hard/lcs.py
@@ -0,0 +1,96 @@
+#coding=utf8
+'''
+求取最大子序列
+'''
+import numpy as np
+import random
+
+def export_x_y(num_x, num_y):
+    '''
+    生成2个随机的序列
+    '''
+    letters = 'ABCD'
+    x = [random.choice(list(letters)) for _ in xrange(num_x)]
+    y = [random.choice(list(letters)) for _ in xrange(num_y)]
+    return x, y
+
+
+def lcs_length(x, y):
+    length_x = len(x)
+    length_y = len(y)
+    c = np.zeros((length_x, length_y))
+    b = c.copy()
+    for i in xrange(1, length_x):
+        for j in xrange(1, length_y):
+            if x[i] == y[j]:
+                c[i][j] = c[i-1][j-1] + 1
+                b[i][j] = ord('\\')
+            elif c[i-1][j] >= c[i][j-1]:
+                c[i][j] = c[i-1][j]
+                b[i][j] = ord('|')
+            else:
+                c[i][j] = c[i][j-1]
+                b[i][j] = ord('-')
+    return c, b
+
+
+def print_lcs(b, x, i, j):
+    result = []
+    def _print_lcs(b, x, i, j, result):
+        if i ==0 or j==0:
+            return
+        if b[i][j] == ord('\\'):
+            result.append(x[i])
+            _print_lcs(b, x, i-1, j-1, result)
+        elif b[i][j] == ord('|'):
+            _print_lcs(b, x, i-1, j, result)
+        elif b[i][j] == ord('-'):
+            _print_lcs(b, x, i, j-1, result)
+    _print_lcs(b, x, i, j, result)
+    return ''.join(result[::-1])
+
+
+def print_lcs_v2(c, x, i, j):
+    result = []
+    def _print_lcs_v2(c, x, i, j, result):
+        if i==0 or j==0:
+            return
+        if c[i][j] == c[i-1][j-1] + 1:
+            result.append(x[i])
+            _print_lcs_v2(c, x, i-1, j-1, result)
+        elif c[i][j] == c[i-1][j]:
+            _print_lcs_v2(c, x, i-1, j, result)
+        elif c[i][j] == c[i][j-1]:
+            _print_lcs_v2(c, x, i, j-1, result)
+    _print_lcs_v2(c, x, i, j, result)
+    return ''.join(result[::-1])
+
+
+def test(count):
+    fail = 0
+    success = 0
+    for i in xrange(count):
+        x, y = export_x_y(11, 10)
+        c, b = lcs_length(x, y)
+        lcs = print_lcs(b, x, len(x)-1, len(y)-1)
+        lcs2 = print_lcs_v2(c, x, len(x)-1, len(y)-1)
+        if not c[-1][-1] == len(lcs) == len(lcs2):
+            fail +=1
+        else:
+            success +=1
+    print('共验算: %s次' % count)
+    print('成功率:%0.2f%%' % (success*100.0/count))
+
+
+if __name__ == '__main__':
+    x, y = export_x_y(11, 10)
+    print(x)
+    print(y)
+    c, b = lcs_length(x, y)
+    print('lcs_length: %s' % c[-1][-1])
+    lcs = print_lcs(b, x, len(x)-1, len(y)-1)
+    print(lcs)
+    lcs = print_lcs_v2(c, x, len(x)-1, len(y)-1)
+    print(lcs)
+    
+    test(1000)
---
+++ hard/regular-expression-matching.py
@@ -0,0 +1,141 @@
+#coding=utf8
+
+'''
+Implement regular expression matching with support for '.' and '*'.
+
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+
+The matching should cover the entire input string (not partial).
+
+The function prototype should be:
+bool isMatch(const char *s, const char *p)
+
+Some examples:
+isMatch("aa","a") → false
+isMatch("aa","aa") → true
+isMatch("aaa","aa") → false
+isMatch("aa", "a*") → true
+isMatch("aa", ".*") → true
+isMatch("ab", ".*") → true
+isMatch("aab", "c*a*b") → true
+'''
+
+'''
+s = 'aab'
+p = 'c*a*b'
+'''
+
+class Solution(object):
+    # def isMatch(self, s, p):
+    #     """
+    #     :type s: str
+    #     :type p: str
+    #     :rtype: bool
+    #     """
+    #     if not (p and s):
+    #         return False
+    #     m = p[0]
+    #     m_next = p[1] if len(p) > 1 else None
+    #     char = s[0]
+    #     match = False
+    #     length = len(s)
+    #
+    #     if m == '.':
+    #         match = True
+    #         if length > 1:
+    #             if m_next:
+    #                 if m_next == '*':
+    #                     return True
+    #                 else:
+    #                     return self.isMatch(s[1:], p[1:])
+    #             else:
+    #                 return False
+    #         else:
+    #             return True
+    #     elif m == char:
+    #         match == True
+    #         if length > 1:
+    #             pass
+    #         else:
+    #             pass
+    #     else:
+    #         if m_next:
+    #             if m_next == '*':
+    #                 return self.isMatch(s, p[2:])
+    #             else:
+    #                 return self.isMatch(s, p[1:])
+    #         else:
+    #             return False
+    cache = {}
+    def isMatch(self, s, p):
+        if (s, p) in self.cache:
+            return self.cache[(s , p)]
+
+        if not p: # 如果p没了,但是还有s,返回Flase
+            return not s
+
+        if p[-1] == '*':
+            if self.isMatch(s, p[:-2]): # '*' Matches zero
+                self.cache[(s, p[:-2])] = True
+                return True
+            if s and (s[-1] == p[-2] or p[-2] == '.') and self.isMatch(s[:-1], p): # '*'' match many
+                self.cache[(s, p)] = True
+                return True
+
+        if s and (s[-1] == p[-1] or p[-1] == '.') and self.isMatch(s[:-1], p[:-1]): # 向左移动p
+            self.cache[(s, p)] = True
+            return True
+        else:
+            self.cache[(s, p)] = False
+            return False
+
+    def isMatch2(self, s, p):
+        """
+        dymatic programming 动态规划
+        dp 保存各组match的记录
+        例如 : s = 'aab', p = 'c*a*b'
+
+                     a  a  b
+               s  0  1  2  3
+            p   --------------
+              0 | 1  0  0  0 |
+            c 1 | 0  0  0  0 |
+            * 2 | 1  0  0  0 |
+            a 3 | 0  1  0  0 |
+            * 4 | 1  1  1  0 |
+            b 5 | 0  0  0  1 |
+
+        db[i+1][j+1]保存的是 s[:i]和p[:j]的结果
+        判断db[i+1][j+1]的结果:
+            if p[i] == '*':
+                pass
+            else:
+                如果 db[i][j] == True
+                如果 p[i] == '.' or p[i] == s[i]
+                则: 真
+        """
+        dp = [[False] * (len(s) + 1) for _ in range(len(p) + 1)]
+        dp[0][0] = True
+        for i in range(1, len(p)):
+            dp[i + 1][0] = dp[i - 1][0] and p[i] == '*'
+        for i in range(len(p)):
+            for j in range(len(s)):
+                if p[i] == '*':
+                    dp[i + 1][j + 1] = dp[i - 1][j + 1] or dp[i][j + 1]
+                    if p[i - 1] == s[j] or p[i - 1] == '.':
+                        dp[i + 1][j + 1] |= dp[i + 1][j]
+                else:
+                    dp[i + 1][j + 1] = dp[i][j] and (p[i] == s[j] or p[i] == '.')
+        for i in dp:
+            print(i)
+        return dp[-1][-1]
+
+
+
+if __name__ == '__main__':
+    s = 'aab'
+    p = 'c*a*b'
+    sol = Solution()
+    result = sol.isMatch2(s, p)
+    print(result)
