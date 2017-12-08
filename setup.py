4f858cc367f925031dda78ab734da13b60205b21
张智超 <wudizhangzhi@163.com>
2017-12-08 14:16:11 +0800

    test release

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
 #coding=utf8
 
-fro distutils.core import setup
+from distutils.core import setup
+
+setup(
+    name = 'LeetCodeCrawler',
+    packages = ['tools'],
+    version = '0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'A LeetCode Crawler',
+    author = 'wudizhangzhi',
+    author_email = '554330595@qq.com',
+    url = 'https://github.com/wudizhangzhi/leetcode',
+    download_url = 'https://github.com/wudizhangzhi/leetcode/archive/0.1.tar.gz',
+    keywords = ['leetcode', 'crawler'],
+    classifiers = [],
+)
+
