d12a1b6951dbecd47e236e26f7775ffb360b35fa
张智超 <wudizhangzhi@163.com>
2017-12-08 17:59:30 +0800

    add gitignore

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
-#coding=utf8
-
+# coding=utf8
+import os
 from distutils.core import setup
 
 setup(
-    name = 'LeetCodeCrawler',
-    packages = ['tools'],
-    version = '0.1',  # Ideally should be same as your GitHub release tag varsion
-    description = 'A LeetCode Crawler',
-    author = 'wudizhangzhi',
-    author_email = '554330595@qq.com',
-    url = 'https://github.com/wudizhangzhi/leetcode',
-    download_url = 'https://github.com/wudizhangzhi/leetcode/archive/0.1.tar.gz',
-    keywords = ['leetcode', 'crawler'],
-    classifiers = [],
+    name='LeetCodeCrawler',
+    packages=['tools'],
+    # packages = os.listdir('tools'),
+    version='0.1.0',  # Ideally should be same as your GitHub release tag varsion
+    description='A LeetCode Crawler',
+    author='wudizhangzhi',
+    author_email='554330595@qq.com',
+    url='https://github.com/wudizhangzhi/leetcode',
+    download_url='https://github.com/wudizhangzhi/leetcode/archive/0.1.0.tar.gz',
+    keywords=['leetcode', 'crawler'],
+    install_requires=['requests>=2.18.4',
+                      'lxml>=3.7.3',
+                      'user-agent>=0.1.9'],
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    license="BSD",
 )
-
