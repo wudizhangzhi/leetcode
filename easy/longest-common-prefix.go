6cdc4f1c9192244c8693b76764014beec00507f2
yueyatianchong <yueyatianchong@qq.com>
2021-09-07 14:06:51 +0800

    update

--- easy/longest-common-prefix.go
+++ easy/longest-common-prefix.go
@@ -3,7 +3,21 @@ package main
 import "fmt"
 
 func longestCommonPrefix(strs []string) string {
-	return ""
+	flag := strs[0]
+	idx := 1
+	for flag != "" && idx < len(strs) {
+		_flag := ""
+		for i, s := range flag {
+			if i < len(strs[idx]) && string(strs[idx][i]) == string(s) {
+				_flag += string(s)
+			} else {
+				break
+			}
+		}
+		flag = _flag
+		idx++
+	}
+	return flag
 }
 
 func main() {
