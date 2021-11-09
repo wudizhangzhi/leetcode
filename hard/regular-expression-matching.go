fecd8ed16da1b8943783952418a8b7d356af050d
yueyatianchong <yueyatianchong@qq.com>
2021-11-09 15:24:05 +0800

    update

--- hard/regular-expression-matching.go
+++ hard/regular-expression-matching.go
@@ -2,8 +2,27 @@ package main
 
 import "fmt"
 
+/*
+动态规划
+dymanic programming
+*/
 func isMatch(s string, p string) bool {
-	return false
+	dp := make([][]bool, len(s)+1)
+	for i := 0; i < len(s)+1; i++ {
+		dp[i] = make([]bool, len(p)+1)
+	}
+	dp[len(s)][len(p)] = true
+	for i := len(s); i > -1; i-- {
+		for j := len(p) - 1; j > -1; j-- {
+			match := i < len(s) && (string(p[j]) == "." || p[j] == s[i])
+			if j+1 < len(p) && string(p[j+1]) == "*" {
+				dp[i][j] = dp[i][j+2] || match && dp[i+1][j]
+			} else {
+				dp[i][j] = match && dp[i+1][j+1]
+			}
+		}
+	}
+	return dp[0][0]
 }
 
 func main() {
