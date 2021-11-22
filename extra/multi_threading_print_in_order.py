7db135c4c1dedbf3e175c122e839d54738cc23dc
wudizhangzhi <yueyatianchong@qq.com>
2021-11-22 22:50:15 +0800

    update

--- extra/multi_threading_print_in_order.py
+++ extra/multi_threading_print_in_order.py
@@ -3,31 +3,40 @@ import threading
 
 def worker(tid:int, lock:threading.Lock, nextLock: threading.Lock):
     i = 0
+    isFirst = True
     while True:
-        lock.acquire()
+        if isFirst and tid == 0:
+            isFirst = False
+        else:
+            lock.acquire()
         print(f"{i}{tid}   线程id: {tid}")
         i+=1
-        lock.acquire()
+        # lock.acquire()
         nextLock.release()
 
 def print_in_order():
     lockList = []
     for _ in range(10):
         l = threading.Lock()
+        # l.acquire(blocking=False)
         lockList.append(l)
 
     pool = []
     for i in range(10):
         t = threading.Thread(
             target=worker,
-            args=(lockList[i], lockList[(i+1)%10])
+            kwargs={
+                "tid": i,
+                "lock": lockList[i], 
+                "nextLock": lockList[(i+1)%10]
+            }
         )
         pool.append(t)
 
-    lockList[0].release()
     for t in pool:
         t.start()
         t.join()
+    
 
 if __name__ == "__main__":
     print_in_order()
