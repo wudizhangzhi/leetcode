307d6fc7dd30e325273880b292f0c18ca835d115
wudizhangzhi <wudizhangzhi@163.com>
2021-11-23 10:00:22 +0800

    update

--- extra/multi_threading_print_in_order.py
+++ extra/multi_threading_print_in_order.py
@@ -1,25 +1,31 @@
 import threading
 
 
-def worker(tid:int, lock:threading.Lock, nextLock: threading.Lock):
+Lock_list = list()
+IsRunning = True
+
+def worker(tid:int):
+    global Lock_list, IsRunning
     i = 0
-    isFirst = True
-    while True:
-        if isFirst and tid == 0:
-            isFirst = False
-        else:
-            lock.acquire()
-        print(f"{i}{tid}   线程id: {tid}")
-        i+=1
-        # lock.acquire()
-        nextLock.release()
+    while IsRunning:
+        if Lock_list[tid]:
+            print(f"{i*10+tid}   线程id: {tid}")
+            if i*10+tid > 10000:
+                IsRunning = False
+                break
+            i+=1
+            Lock_list[tid] = False
+            Lock_list[(tid+1)%10] = True
 
 def print_in_order():
-    lockList = []
-    for _ in range(10):
-        l = threading.Lock()
-        # l.acquire(blocking=False)
-        lockList.append(l)
+    for i in range(10):
+        # l = threading.Lock()
+        # # l.acquire(blocking=False)
+        # lockList.append(l)
+        if i==0:
+            Lock_list.append(True)
+        else:
+            Lock_list.append(False)
 
     pool = []
     for i in range(10):
@@ -27,8 +33,6 @@ def print_in_order():
             target=worker,
             kwargs={
                 "tid": i,
-                "lock": lockList[i], 
-                "nextLock": lockList[(i+1)%10]
             }
         )
         pool.append(t)
